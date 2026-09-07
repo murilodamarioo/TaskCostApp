1640c27a00577e9fcc0008fa0c983598983dc216
Murilo Damario <murilo.gcdamario@gmail.com>
2026-09-07 19:04:40 -0300

    :tada: feat: update dependencies and add Tailwind CSS configuration
    - Added babel-plugin-module-resolver and babel-preset-expo to dependencies.
    - Included nativewind, prettier-plugin-tailwindcss, react-native-reanimated, react-native-safe-area-context, react-native-worklets, and tailwindcss.
    - Created a new colors.ts file for shared color definitions.
    - Added global CSS file for Tailwind CSS styles.
    - Configured Tailwind CSS in tailwind.config.js with custom theme settings.

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+import './src/styles/global.css'
 
 export default function App() {
   return (
