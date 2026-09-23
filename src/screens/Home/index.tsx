b9f1b354d330d2d3ff5ccefd661a10d28accf138
Murilo Damario <murilo.gcdamario@gmail.com>
2026-09-22 21:49:53 -0300

    :sparkles:feat: add Summary screen with Header component

--- src/screens/Home/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native'
-
-export const Home = () => {
-  return (
-    <View className='flex-1 justify-center items-center'>
-      <Text className='text-base color-slate-950'>Home</Text>
-    </View>
-  )
-}
